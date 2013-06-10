53d3f5f224c2b40d28dac800a2f2f9c8dcfea6f3
OpenShift guest <olearym@mit.edu>
2013-06-10 14:42:33 -0400

    afternoon work

--- day1.js
+++ day1.js
@@ -1,6 +1,74 @@
-function calculate() {
-    var input = $('#text1:first');
-    var val = input.val();
-    var output = $('#text1_out:first')
-    output.text(val);
+function calculate(text) {
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g;
+    var tokens = text.match(pattern);
+    tokens = JSON.stringify(tokens);
+    try {
+        var val = evaluate(tokens);
+        if(tokens.length > 0) {
+            throw "ill-formed expression";
+        }
+        return String(val);
+    } catch(err) {
+        return err;
+    }
 }
+
+function setup_calc(div) {
+    var input = $('<input></input>', {type: "text", size: 50});
+    var output = $("<div></div>");
+    var button = $("<button>Calculate</button>");
+    button.bind("click", function () {
+        output.text(String(calculate(input.val())));
+    });
+    
+    $(div).append(input, button, output);
+}
+
+function read_operand(array) {
+    var num = array[0];
+    array.shift();
+    if(isNaN(parseInt(num, 10))) {
+        throw "number expected";
+    } else {
+        return parseInt(num, 10);
+    }
+}
+
+function evaluate(array) {
+    if(array.length === 0) {
+        throw "missing operand";
+    }
+    var value = read_operand(array);
+    var patterns = /\+|\-|\*|\//g;
+    while(array.length > 0) {
+        var operator = array[0];
+        array.shift();
+        if(operator.match(patterns) === null) {
+            throw "unrecognized operator";
+        }
+        if(array.length === 0) {
+            throw "missing operand";
+        }
+        var temp = read_operand(array);
+        if(operator === "+") {
+            value = value + temp;
+        }
+        else if(operator === "-") {
+            value = value - temp;
+        }
+        else if(operator === "*") {
+            value = value * temp;
+        }
+        else if(operator === "/") {
+            value = value / temp;
+        }
+    }
+    return value;
+}
+
+$(document).ready(function (){
+    $('.calculator').each(function (){
+        //this refers to the <div> with class calculator
+        setup_calc(this);
+    });
+});
